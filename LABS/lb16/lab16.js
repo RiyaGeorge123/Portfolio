50e6ae28e9400b78189f3b4496be7daef842d901
RiyaGeorge123 <87453762+RiyaGeorge123@users.noreply.github.com>
2021-08-17 19:59:32 -0400

    Add files via upload

---
+++ LABS/lb16/lab16.js
@@ -0,0 +1,14 @@
+var p= prompt("Please enter your name", );
+alert("Hello," + " " + p + " want to find out how old you'll be in any future year ? ")
+var answer= prompt("Enter yes or no ");
+ if (answer == "yes") {
+   var birthyear = prompt("Enter the year you were born: ")
+   var futureyear = prompt("Enter the future year you want to find out ")
+   var age= futureyear - birthyear;
+   var estimate= age - 1;
+  alert(" You will be either "+ " " + age + " or " + estimate + " years old in the year " + " " + futureyear " ! ")
+}
+
+else if( answer == "no"){
+   alert(" Ok, Bye ")
+ }
